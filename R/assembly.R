52aeaed9a186bdbf31d44ae83b8ca1bd525650fa
Zhixing Feng <zxfeng.thu@gmail.com>
2018-01-17 09:53:38 -0500

    add assemble.R and kmeans_clique.R

---
+++ R/assembly.R
@@ -0,0 +1,186 @@
+assemble.trim.centroid <- function(m5.data.sub, centroid, centroid.range, min.cvg = 20)
+{
+	# if centroid has no variants quit
+	if (length(centroid)==0)
+		return(NULL)
+
+	# if centroid has only 1 variant, trim it directly
+	rl <- list()
+	if (length(centroid)==1){
+		rl[[1]] <- assemble.trim.centroid.core(m5.data.sub, centroid, centroid.range, min.cvg)
+		return(rl)
+	}
+	
+	# if centroid has multiple variants, scan it and trim. 
+	cut.points.left <- floor(centroid[-length(centroid)]/4)
+	cut.points.right <- floor(centroid[-1]/4)
+	cut.points <- floor((cut.points.right + cut.points.left)/2) 
+	cut.points <- c(centroid.range[1], cut.points)
+	for (i in 1:length(cut.points)){
+		cur.centroid.range <- c(cut.points[i], centroid.range[2])
+		cur.centroid <- centroid[centroid >= 4*cur.centroid.range[1] & centroid <= (4*cur.centroid.range[2]+3)]
+		cur.rl <- assemble.trim.centroid.core(m5.data.sub, cur.centroid, cur.centroid.range, min.cvg)
+		if (is.null(cur.rl))
+			break
+		rl[[i]] <- assemble.trim.centroid.core(m5.data.sub, cur.centroid, cur.centroid.range, min.cvg)
+		if (rl[[i]]$centroid.range.trim[2] == centroid.range[2])
+			break
+	}
+	rl
+}
+assemble.trim.centroid.core <- function(m5.data.sub, centroid, centroid.range, min.cvg = 20)
+{
+	cvg.profile <- rep(0, 4*max(m5.data.sub$tEnd) + 3 + 1)	
+	idx <- which(m5.data.sub$tStart <= centroid.range[1] & m5.data.sub$tEnd > centroid.range[1])
+	for (i in idx){
+		cur.loci <- (4*m5.data.sub$tStart[i] + 1) : (4*m5.data.sub$tEnd[i] + 3 + 1)
+		cvg.profile[cur.loci] <- cvg.profile[cur.loci] + 1
+	}
+	
+	if (sum(cvg.profile >= min.cvg) == 0)
+		return(NULL)
+
+	centroid.range.upper <- floor((max(which(cvg.profile >= min.cvg)) - 1) / 4)
+	if (centroid.range[1] >= centroid.range.upper)
+		return(NULL)
+	centroid.range.trim <- c(centroid.range[1], min(centroid.range.upper, centroid.range[2]))
+        centroid.trim <- centroid[centroid >= 4*centroid.range.trim[1] & centroid <= 4*centroid.range.trim[2] + 3]
+        if (length(centroid.trim)==0)
+		return(NULL)
+	list(centroid.trim = centroid.trim, centroid.range.trim = centroid.range.trim)
+}
+
+assemble <- function(encode.data, m5.data, centroid, centroid.range, min.cvg = 20, min.overlap = 200, 
+		max.iter = 50, is.trim=TRUE)
+{
+	#------------- check input data ------------#
+        if (length(encode.data)!=nrow(m5.data)){
+                stop('encode.data and m5.data do not match.')
+        }
+
+	if (length(centroid)==0)
+		return(NULL)
+	if (any(centroid < 4*centroid.range[1] | centroid > (4*centroid.range[2]+3)))
+		stop('centroid and centroi.range do not match')	
+
+	#------------- update centroid -----------#
+	encode.data.sub <- encode.data
+        m5.data.sub <- m5.data
+        centroid.update <- centroid
+        centroid.range.update <- centroid.range
+	for (i in 1:max.iter){
+		rl.core <- assemble.core(encode.data.sub, m5.data.sub,
+                                        centroid.update, centroid.range.update,
+                                        min.overlap = min.overlap)
+		if (length(rl.core$centroid)==0 | identical(rl.core$centroid, centroid.update))	
+			break
+		centroid.update <- rl.core$centroid
+		centroid.range.update <- rl.core$centroid.range
+	}
+	rl.core[['n.iter']] <- i
+
+	#------------- trim centroid -------------#
+	if (is.trim){	
+		if (length(rl.core$idx.on) < min.cvg)
+			return(rl.core)
+		rl.trim <- assemble.trim.centroid(m5.data[rl.core$idx.on,], rl.core$centroid, rl.core$centroid.range, min.cvg)
+		rl.core[['trim']] <- rl.trim
+	}
+	rl.core[['centroid.init']] <- centroid
+	rl.core
+}
+
+assemble.left <- function(encode.data, m5.data, centroid, centroid.range, min.cvg = 20, min.overlap = 200, max.iter = 50)
+{
+	 #------------- check input data ------------#
+        if (length(encode.data)!=nrow(m5.data)){
+                stop('encode.data and m5.data do not match.')
+        }
+
+	idx.on <- 1:length(encode.data)
+	for (i in 1:max.iter){
+		# trim centroid
+		idx <- idx.on[m5.data$tStart[idx.on] <= centroid.range[1] & m5.data$tEnd[idx.on] > centroid.range[1]]
+		
+		cvg.profile <- rep(0, 4*centroid.range[2] + 3 - 4*centroid.range[1] + 1)
+		for (j in idx){
+			cur.start <- max(m5.data$tStart[j], centroid.range[1])
+			cur.end <- min(m5.data$tEnd[j], centroid.range[2])
+			cur.loci <- (4*(cur.start - centroid.range[1]) + 1) : (4*(cur.end - centroid.range[1]) + 3 + 1)
+			cvg.profile[cur.loci] <- cvg.profile[cur.loci] + 1
+		}
+		if (sum(cvg.profile >= min.cvg) == 0)
+			break
+		centroid.range.upper <- floor((max(which(cvg.profile >= min.cvg)) - 1 + 4*centroid.range[1]) / 4)
+		centroid.range <- c(centroid.range[1], min(centroid.range.upper, centroid.range[2]))
+		centroid <- centroid[centroid >= 4*centroid.range[1] & centroid <= 4*centroid.range[2] + 3]	
+		if (length(centroid)==0)
+			break
+
+		# update centroid
+		rl.core <- assemble.core(encode.data, m5.data, centroid, centroid.range, min.overlap = min.overlap)
+		idx.on <- rl.core$idx.on
+		if (identical(centroid, rl.core$centroid))
+			break
+		centroid <- rl.core$centroid
+	}
+
+	list(centroid = centroid, centroid.range = centroid.range, idx.on = idx.on, n.iter = i)
+}
+
+assemble.core <- function(encode.data, m5.data, centroid, centroid.range, min.overlap=200)
+{
+	#------------- check input data ------------#
+	if (length(encode.data)!=nrow(m5.data)){
+                stop('encode.data and m5.data do not match.')
+        }
+
+	#------------ match reads to centroids ----------#
+        is.on <- rep(FALSE, length(encode.data))
+        for (i in 1:length(encode.data)){
+		overlap.start <- max(centroid.range[1], m5.data$tStart[i])
+		overlap.end <- min(centroid.range[2], m5.data$tEnd[i])
+		overlap.len <- overlap.end - overlap.start + 1
+		if (overlap.len < min.overlap)
+			next
+
+		common.var <- intersect(encode.data[[i]], centroid)
+                n.centroid.overlap <- sum(centroid>=4*m5.data$tStart[i] & centroid<=4*m5.data$tEnd[i]+3)
+                if (length(common.var)>n.centroid.overlap)
+                	stop('length(common.var)>n.centroid.overlap')
+                if (length(common.var) >= ceiling(n.centroid.overlap/2) & n.centroid.overlap > 0)
+                        is.on[i] <- TRUE
+        }
+        idx.on <- which(is.on)	
+
+	#------------- update centroid -------------#
+	new.centroid <- centroid
+	cur.encode.data <- encode.data[idx.on]
+	cur.m5.data <- m5.data[idx.on,]
+	
+	cur.max.encode <- max(4*m5.data$tEnd+3)
+        cur.var.count <- integer(cur.max.encode + 1)
+        cur.read.count <- integer(cur.max.encode + 1)
+
+	# count number of variants and coverage for each locus          
+        for (i in 1:length(cur.encode.data)){
+		if (length(cur.encode.data[[i]]) == 0)
+			next
+                cur.var.count[cur.encode.data[[i]] + 1] <- cur.var.count[cur.encode.data[[i]] + 1] + 1
+        }
+
+        # count coverage for each locus
+        for (i in 1:nrow(cur.m5.data)){
+                cur.read.count[(4*cur.m5.data$tStart[i]+1):(4*cur.m5.data$tEnd[i]+3+1)] <-
+                	cur.read.count[(4*cur.m5.data$tStart[i]+1):(4*cur.m5.data$tEnd[i]+3+1)] + 1
+        }
+
+        # majority voting
+        new.centroid <- which(2*cur.var.count>=cur.read.count & cur.read.count>0) - 1
+	
+	# restrict new.centroid to centroid.range
+	new.centroid <- new.centroid[new.centroid >= 4*centroid.range[1] & new.centroid <= 4*centroid.range[2]+3]
+
+	list(centroid = new.centroid, centroid.range = centroid.range,idx.on = idx.on)
+}
+
---
+++ R/kmeans_clique.R
@@ -0,0 +1,167 @@
+find_contained_reads <- function(encode.data, m5.data, min.overlap.var = 1, min.overlap.len=200)
+{
+	###--------- check if encode.data and m5.data match --------###
+        if (length(encode.data)!=nrow(m5.data)){
+                stop('encode.data and m5.data do not match.')
+        }
+
+        ###--------- remove reads contained in another read ------###
+        read.pair.mat <- matrix(FALSE, length(encode.data), length(encode.data))
+        for (i in 1:length(encode.data)){
+                print(i)
+                for (j in 1:length(encode.data)){
+                        # don't compare reads to themselves
+			if (i==j) next
+			# don't compare non-overlaping reads
+                        if (m5.data$tStart[i] > m5.data$tEnd[j] | m5.data$tEnd[i] < m5.data$tStart[j])
+                                next
+			# get overlap region
+			overlap.start <- max(m5.data$tStart[i], m5.data$tStart[j])	
+			overlap.end <- min(m5.data$tEnd[i], m5.data$tEnd[j])
+			overlap.len <- overlap.end - overlap.start + 1
+			if (overlap.len < min.overlap.len)
+				next
+			
+			# get number of common variants
+			n.common.var <- length(intersect(encode.data[[i]], encode.data[[j]]))
+			
+			# get number of variants of reads i and j in overlaping region
+			n.var.overlap.i <- sum(encode.data[[i]]>=4*overlap.start & encode.data[[i]]<=4*overlap.end+3)
+			n.var.overlap.j <- sum(encode.data[[j]]>=4*overlap.start & encode.data[[j]]<=4*overlap.end+3)
+			
+				
+                        if (n.var.overlap.i > min.overlap.var & n.var.overlap.j > min.overlap.var & 
+				n.common.var >= ceiling(n.var.overlap.i/2) & n.common.var >= ceiling(n.var.overlap.j/2)){
+                                read.pair.mat[i,j] <- TRUE
+                        }
+                }
+        }
+        read.pair.mat
+	
+}
+
+
+kmeans_reads <- function(encode.data, m5.data, centroid.seed, centroid.seed.range, min.cvg=20, min.overlap=200, max.iter=200)
+{
+	centroid.seed.update <- centroid.seed
+	centroid.seed.range.update <- centroid.seed.range
+	n.iter <- 0
+	for (i in 1:max.iter){
+		rl.core <- kmeans_reads_core(encode.data, m5.data, centroid.seed.update, centroid.seed.range.update,
+					 min.cvg, min.overlap)
+		if (identical(centroid.seed.update, rl.core$new.centroid))
+			break
+		centroid.seed.update <- rl.core$new.centroid
+		centroid.seed.range.update <- rl.core$new.centroid.range
+		n.iter <- n.iter + 1			
+	}
+	list(centroid = centroid.seed.update, centroid.range = centroid.seed.range.update, 
+		centroid.read.id = rl.core$centroid.read.id, n.iter = n.iter)
+						
+}
+
+kmeans_reads_core <- function(encode.data, m5.data, centroid.seed, centroid.seed.range, min.cvg=20, min.overlap=200)
+{
+	###--------- match reads to the centroid --------###
+        if (length(encode.data)!=nrow(m5.data)){
+                stop('encode.data and m5.data do not match.')
+        }
+	if (length(centroid.seed) != length(centroid.seed.range)) 
+		stop('length(centroid.seed) != length(centroid.seed.range)')       
+
+	group.id <- list() # 0 means consensus sequence (no variant)
+	centroid.read.id <- lapply(centroid.seed, function(x) integer(0))
+	for (i in 1:length(encode.data)){
+                cur.jaccard <- rep(0, length(centroid.seed))
+		cur.rate <- rep(0, length(centroid.seed))
+		for (j in 1:length(centroid.seed)){
+			overlap.start <- max(m5.data$tStart[i], centroid.seed.range[[j]][1])
+			overlap.end <- min(m5.data$tEnd[i],  centroid.seed.range[[j]][2])	
+			if (overlap.end - overlap.start + 1 < min.overlap)
+				next
+			
+			cur.encode <- encode.data[[i]][encode.data[[i]] >= 4*overlap.start & encode.data[[i]] <= 4*overlap.end+3]
+			cur.centroid <- centroid.seed[[j]][centroid.seed[[j]] >= 4*overlap.start & centroid.seed[[j]] <= 4*overlap.end+3]
+			cur.common <- intersect(cur.encode, cur.centroid)
+			cur.union <- union(cur.encode, cur.centroid)			
+
+			# calculate jaccard index
+			if (length(cur.union) > 0)
+				cur.jaccard[j] <- length(cur.common) / length(cur.union)
+			else
+				cur.jaccard[j] <- 0
+			
+			# calculate proportion of shared variants in centroid
+			if (length(cur.centroid) > 0)
+				cur.rate[j] <- length(cur.common) / length(cur.centroid)
+			else
+				cur.rate[j] <- 0
+				 
+		}
+		
+		idx.on <- which(cur.rate>=0.5)
+		if (length(idx.on) == 0){
+                        group.id[[i]] <- 0
+		}else{
+			cur.jaccard.max <- max(cur.jaccard[idx.on])
+			idx.jaccard.max <- idx.on[abs(cur.jaccard[idx.on] - cur.jaccard.max) <= 1e-12]
+			group.id[[i]] <- idx.jaccard.max 
+			for (j in 1:length(idx.jaccard.max))
+				centroid.read.id[[idx.jaccard.max[j]]] <- c(centroid.read.id[[idx.jaccard.max[j]]], i)
+		}
+			
+        }
+
+	# update centroid accordingly
+	new.centroid <- centroid.seed
+	new.centroid.range <- centroid.seed.range
+	for (i in 1:length(centroid.read.id)){
+		if (length(centroid.read.id[[i]])==0){
+			new.centroid[[i]] <- integer()
+			new.centroid.range[[i]] <- c(0,0)
+			break
+		}
+		cur.encode.data <- encode.data[centroid.read.id[[i]]]
+		cur.m5.data <- m5.data[centroid.read.id[[i]], ]
+		
+		cur.max.encode <- max(4*m5.data$tEnd+3)
+                cur.var.count <- integer(cur.max.encode + 1)
+                cur.read.count <- integer(cur.max.encode + 1)
+
+		# count number of variants and coverage for each locus		
+		for (j in 1:length(cur.encode.data)){
+			if (length(cur.encode.data[[j]]) == 0)
+				next
+			cur.var.count[cur.encode.data[[j]] + 1] <- cur.var.count[cur.encode.data[[j]] + 1] + 1
+		}
+		
+		# count coverage for each locus
+		for (j in 1:nrow(cur.m5.data)){
+			cur.read.count[(4*cur.m5.data$tStart[j]+1):(4*cur.m5.data$tEnd[j]+3+1)] <- 
+				cur.read.count[(4*cur.m5.data$tStart[j]+1):(4*cur.m5.data$tEnd[j]+3+1)] + 1
+		}		
+	
+			
+		# majority voting
+		new.centroid[[i]] <- which(2*cur.var.count>=cur.read.count) - 1
+
+		# trim low coverage ends
+		low.cvg <- which(cur.read.count < min.cvg) - 1
+		new.centroid[[i]] <- setdiff(new.centroid[[i]], low.cvg)
+
+		# update range
+		high.cvg <- which(cur.read.count >= min.cvg) - 1
+		if (length(high.cvg) > 0){
+			new.centroid.range[[i]] <- floor(range(high.cvg)/4)
+		}else{
+			new.centroid.range[[i]] <- c(0,0)
+		}
+		#new.centroid.range[[i]] <- floor(range(new.centroid[[i]])/4)
+	}
+
+	list(new.centroid=new.centroid, new.centroid.range=new.centroid.range, centroid.read.id=centroid.read.id)	
+}
+
+
+
+
