b2a3170c169bea20cd6b85c5d1f282ead67008a2
Zhixing Feng <zxfeng.thu@gmail.com>
2018-05-31 11:20:29 -0400

    add conv_encode_to_mat

--- R/format.R
+++ R/format.R
@@ -1,6 +1,14 @@
 conv_encode_to_mat <- function(encode.data)
 {
-
+	var.set <- sort(unique(unlist(encode.data)))
+	encode.mat <- matrix(0, nrow = length(encode.data), ncol = length(var.set))
+	if (nrow(encode.mat)==0 | ncol(encode.mat)==0)
+		return(NULL)
+	encode.data.match <- lapply(encode.data, function(x,t) match(x,t), t = var.set)
+	for (i in 1:length(encode.data.match)){
+		encode.mat[i,encode.data.match[[i]]] <- 1
+	}
+	encode.mat
 }
 
 
